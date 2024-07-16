aaf36cb043db08e999d3e6dddbdad34f749b3458
jgwng <gunwng123@gmail.com>
2024-07-16 22:41:10 +0900

    update :Semantic ID 편하게 설정할 수 있도록 extension 추가

--- client/lib/util/test_util.dart
+++ client/lib/util/test_util.dart
@@ -1,3 +1,7 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
 class MaestroTestUtil{
 
   List<K>? jsonToList<K>(Map<String, dynamic>? json,
@@ -8,5 +12,34 @@ class MaestroTestUtil{
     if (json.isEmpty || !json.containsKey(key)) return [];
 
     return List<K>.from(json[key].map((model) => fromJson(model)));
+  }
+}
+
+extension WidgetExtension on Widget {
+  Widget get toSemanticExplicit => Semantics(
+    explicitChildNodes: true,
+    child: this,
+  );
+
+  Widget toSemantic({required String id,bool? enabled,bool? checked,String? label}){
+    return Semantics(
+      identifier: id,
+      enabled: enabled,
+      checked: checked,
+      label: label,
+      child: Tooltip(
+        message: id,
+        child: Listener(
+          onPointerDown: (event) async{
+            // Check if right mouse button clicked
+            if (event.kind == PointerDeviceKind.mouse && event.buttons == kSecondaryMouseButton) {
+              print('right mouse click');
+              await Clipboard.setData(const ClipboardData(text: "Text to be copied"));
+            }
+          },
+          child: this,
+        ),
+      ),
+    );
   }
 }
